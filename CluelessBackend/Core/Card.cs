d9a1ca24cfb1a3d4fc83b329308f3bc1fa92d770
Amarilda <amarildacelhaka@gmail.com>
2021-03-20 23:12:39 -0500

    Creating a deck of cards, with card type and card name

--- CluelessBackend/Core/Card.cs
+++ CluelessBackend/Core/Card.cs
@@ -8,18 +8,62 @@ namespace CluelessBackend.Core
 {
     class Card
     {
-        enum CardType
+        public enum CARD_TYPE
         {
-            Suspect,
-            Weapon,
-            Room
+            SUSPECT,
+            WEAPON,
+            ROOM
         }
-        
+
+        public enum WEAPON_CARDS
+        {
+            ROPE,
+            LEAD_PIPE,
+            KNIFE,
+            WRENCH,
+            CANDLESTICK,
+            REVOLVER
+        }
+
+        public enum SUSPECT_CARDS
+        {
+            COLONEL_MUSTARD,
+            MISSS_SCARLET,
+            PREOFESSOR_PLUM,
+            MR_GREEN,
+            MRS_WHITE,
+            MRS_PEACOCK
+        }
+
+        public enum ROOM_CARDS
+        {
+            STUDY,
+            HALL,
+            LOUNGE,
+            LIBRARY,
+            BILLIARD_ROOM,
+            DINNING_ROOM,
+            CONSERVATORY,
+            BALLROOM,
+            KITHCEN
+        }
+
+        // TODO:: Maybe remove this
         string[] weaponCards = { "Rope", "Lead Pipe", "Knife", "Wrench", "Candlestick", "Revolver" };
         string[] suspectCards = { "Colonel Mustard", "Miss Scarlet", "Professor Plum", "Mr. Green",
                 "Mrs. White", "Mrs. Peacock"};
         string[] roomCards = {"Study", "Hall", "Lounge", "Library", "Billiard Room",
         "Dining Room", "Conservatory", "Ballroom", "Kitchen"};
 
+        // Getter and setter functions for the card enums 
+        public CARD_TYPE Card_Type { get; set; }
+        public WEAPON_CARDS Weapon_Cards { get; set; }
+        public SUSPECT_CARDS Suspect_Cards { get; set; }
+        public ROOM_CARDS Room_Cards { get; set; }
+
+
     }
+
+
 }
+
---
+++ CluelessBackend/Core/CardDeck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CluelessBackend.Core
+{
+    class CardDeck : Card
+    {
+        // Number of cards = 6 weapons + 6 suspects + 9 rooms
+        const int CARD_NUMBER = 21;
+
+        // array that will store all the cards (weapons, suspects and rooms)
+        private Card[] deckOfCards_;
+
+        public CardDeck()
+        {
+            deckOfCards_ = new Card[CARD_NUMBER];
+        }
+
+        public void CreateDeckOfCards()
+        {
+            int cardIndex = 0;
+            foreach (WEAPON_CARDS weaponIndex in Enum.GetValues(typeof(WEAPON_CARDS)))
+            {
+                deckOfCards_[cardIndex] = new Card { Card_Type = CARD_TYPE.WEAPON, Weapon_Cards = weaponIndex };
+                cardIndex++;
+            }
+
+            foreach (SUSPECT_CARDS suspectIndex in Enum.GetValues(typeof(SUSPECT_CARDS)))
+            {
+                deckOfCards_[cardIndex] = new Card { Card_Type = CARD_TYPE.SUSPECT, Suspect_Cards = suspectIndex };
+                cardIndex++;
+            }
+
+            foreach (ROOM_CARDS roomIndex in Enum.GetValues(typeof(ROOM_CARDS)))
+            {
+                deckOfCards_[cardIndex] = new Card { Card_Type = CARD_TYPE.ROOM, Room_Cards = roomIndex };
+                cardIndex++;
+            }
+        }
+    }
+}
