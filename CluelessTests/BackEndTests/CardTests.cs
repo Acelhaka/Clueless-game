c2fffaededf2bdfc74353518fb4d37e08372cdfd
brendan <bdunn314@gmail.com>
2021-03-23 21:09:52 -0500

    adding unit tests for the CardDeck object

--- CluelessTests/BackEndTests/CardTests.cs
+++ CluelessTests/BackEndTests/CardTests.cs
@@ -7,17 +7,59 @@ using CluelessBackend;
 using CluelessBackend.Core;
 using FluentAssertions;
 using Xunit;
+using System.IO;
+using CluelessNetwork;
+using CluelessNetwork.NetworkSerialization;
+using FluentAssertions;
+using Xunit;
+
 
 namespace CluelessTests.BackEndTests
 {
     public class CardTests
     {
 
+        [Fact]
         public void CreateDeckOfCardsTest()
         {
             CardDeck deck = new CardDeck();
             deck.CreateDeckOfCards();
-          //  Assert.Equal(deck.cardDeck )
+            int deckSize = deck.GetDeckSize();
+            // there should be a total of 21 cards in the deck (9 rooms + 6 weapons + 6 suspects)
+            Assert.True(deckSize.Equals(21));
+
+            // this test will ensure the case file generated pulls one suspect, one weapon and one room only
+            Card[] caseFile = deck.SelectCardsForEnvelope();
+
+            bool weaponFound = false;
+            bool suspectFound = false;
+            bool roomFound = false;
+
+            // first test to make sure the case file has only three cards in it
+            Assert.True(caseFile.Length.Equals(3));
+
+            for (int i = 0; i < caseFile.Length; i++) {
+                Card c = caseFile[i];
+                if (c.Card_Type.Equals(Card.CARD_TYPE.WEAPON)) {
+                    weaponFound = true;
+                } else if (c.Card_Type.Equals(Card.CARD_TYPE.ROOM))
+                {
+                    roomFound = true;
+                } else if (c.Card_Type.Equals(Card.CARD_TYPE.SUSPECT))
+                {
+                    suspectFound = true;
+                }
+            }
+
+            // test that each card type flag is now true after going through the case file
+            Assert.True(weaponFound.Equals(true));
+            Assert.True(suspectFound.Equals(true));
+            Assert.True(roomFound.Equals(true));
+
+            // now test that the cards from the case file are not in the regular deck that will be dealt to the players
+            
+
+
         }
 
     }
